9682daee29cda3fb6199b5aff62567dc6e9c946a
George Fourtounis <gfour@admin.grnet.gr>
2025-12-10 14:44:08 +0000

    Fix deployment settings

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ fi
 source .venv/bin/activate
 export FLASK_RUN_PORT=5602
 export ISSUER_URL=https://snf-74864.ok-kno.grnetcloud.net:5600/
-export SERVICE_URL=https://snf-74864.ok-kno.grnetcloud.net:5602/
+export SERVICE_URL=https://snf-74864.ok-kno.grnetcloud.net:5602
 
 echo "Running in branch: "$(git rev-parse --abbrev-ref HEAD)
 flask --app app run --debug --cert=/etc/letsencrypt/live/snf-74864.ok-kno.grnetcloud.net/fullchain.pem --key=/etc/letsencrypt/live/snf-74864.ok-kno.grnetcloud.net/privkey.pem --host="$HOST" --port ${FLASK_RUN_PORT}
